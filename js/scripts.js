3930841c7071db72ad1edfb8ae1877ff760e529a
IuCC <80768059+IuCC123@users.noreply.github.com>
2022-04-22 16:18:53 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
-function serverListener1() {
+var request = new XMLHttpRequest();
+request.onload = requestListener;
+request.onerror = requestError;
+request.open('get', 'https://api.mcsrvstat.us/2/unreachpvp.servers.unreach.host', true);
+request.send();
+function requestListener() {
   var data = JSON.parse(this.responseText);
   console.log(data);
   document.getElementById('server-1-motd').innerHTML = data.motd.html
   document.getElementById('server-1-online').innerHTML = data.players.online
   document.getElementById('server-1-max').innerHTML = data.players.max
 }
-var request = new XMLHttpRequest();
-request.onload = serverListener1;
-request.onerror = requestError;
-request.open('get', 'https://adminpanel.cyclenet.eu/api/ping?ip=unreachpvp.servers.unreach.host&port=25565', true);
-request.send();
+function requestError(){
+    return;
+}
