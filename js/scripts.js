974ce5c5d73c536a4bdcf9238b3803d8ab336b41
AngerminecraftYT <83855262+AngerminecraftYT@users.noreply.github.com>
2022-04-25 07:36:17 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,21 +13,3 @@ function requestListener() {
 function requestError(){
     return;
 }
-
-// NEXT
-
-var request = new XMLHttpRequest();
-request.onload = requestListener;
-request.onerror = requestError;
-request.open('get', 'https://api.mcsrvstat.us/2/UHHubGT.servers.unreach.host', true);
-request.send();
-function requestListener() {
-  var data = JSON.parse(this.responseText);
-  console.log(data);
-  document.getElementById('server-2-motd').innerHTML = data.motd.html
-  document.getElementById('server-2-online').innerHTML = data.players.online
-  document.getElementById('server-2-max').innerHTML = data.players.max
-}
-function requestError(){
-    return;
-}
